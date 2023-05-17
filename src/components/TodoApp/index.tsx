0c3b2251b1d653199b3cd0233711c733ed525a12
patr4519 <jero96@mail.ru>
2023-05-17 21:56:28 +0300

    +

--- src/components/TodoApp/index.tsx
+++ src/components/TodoApp/index.tsx
@@ -1,11 +1,11 @@
-import styles from './TodoApp.module.scss'
+import styles from "./TodoApp.module.scss";
 
 const TodoApp = () => {
-    return (
-        <div className={styles.todo_app}>
-            TodoApp
-        </div>
-    )
-}
+  return (
+    <div className={styles.todo_app_wrapper}>
+      <div className={styles.todos}>todos</div>
+    </div>
+  );
+};
 
 export default TodoApp;
