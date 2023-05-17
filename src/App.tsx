2d85328b0a377d68b80d80b14756a6d3f73d0d55
patr4519 <jero96@mail.ru>
2023-05-17 20:18:12 +0300

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+       {/* <Header /> */}
+       <div className='contnet'>
+          <Routes>
+            {/* <Route path='/' element={<Home />} /> */}
+            {/* <Route path='/profile' element={<Profile />} /> */}
+          </Routes>
+       </div>
+       {/* <Footer /> */}
     </div>
   );
 }
