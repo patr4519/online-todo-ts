cd410ba1a75619c2d0bd05ff7486d0f0dc4ead2d
patr4519 <jero96@mail.ru>
2023-05-18 21:26:19 +0300

    +

--- src/components/LiTodo/index.tsx
+++ src/components/LiTodo/index.tsx
@@ -1,26 +1,40 @@
+import styles from "./LiTodo.module.scss";
 import { useAppDispatch } from "../../app/hooks";
 import { changeComplete, removeItem } from "../../features/todos/todosSlice";
 import { ItemTodo } from "../../types/data";
-import styles from "./LiTodo.module.scss";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
 
 const LiTodo = (props: ItemTodo) => {
   const dispath = useAppDispatch();
 
   return (
     <li>
-      <input
-        onClick={() => dispath(changeComplete(props.id))}
-        defaultChecked={props.completed}
-        type="checkbox"
-      />
-      <label>{props.text}</label>
-      <button onClick={() => alert(props.description)}>Description</button>
-      <DeleteIcon
-        className={styles.deleteIcon}
-        onClick={() => dispath(removeItem(props.id))}
-        sx={{ color: "#e32315" }}
-      />
+      <div className={styles.leftBlock}>
+        <input
+          onClick={() => dispath(changeComplete(props.id))}
+          defaultChecked={props.completed}
+          type="checkbox"
+        />
+        <label>{props.text}</label>
+      </div>
+      <div className={styles.rightBlock}>
+        {props.description && (
+          <Button
+            onClick={() => alert(props.description)}
+            size="small"
+            variant="contained"
+            color="success"
+          >
+            Desc
+          </Button>
+        )}
+        <DeleteIcon
+          className={styles.deleteIcon}
+          onClick={() => dispath(removeItem(props.id))}
+          sx={{ color: "#e32315" }}
+        />
+      </div>
     </li>
   );
 };
